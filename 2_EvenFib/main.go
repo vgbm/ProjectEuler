e0cb667b48bfd444b4bf7eeb0b6e450053ce1353
James <james.thomas.vaughn@gmail.com>
2017-08-28 15:52:26 -0400

    Completed challenge 2

--- 2_EvenFib/main.go
+++ 2_EvenFib/main.go
@@ -1,4 +1,22 @@
 package main
 
 func main() {
+	print(SumOfEvenFibsUnder(4000000))
+}
+
+func SumOfEvenFibsUnder(upperLimit int) int {
+	prev := 0
+	curr := 1
+	sum := 0
+
+	for curr <= upperLimit {
+		if curr%2 == 0 {
+			sum += curr
+		}
+
+		//next iter of fib nums
+		prev, curr = curr, curr+prev
+	}
+
+	return sum
 }
--- 2_EvenFib/main_test.go
+++ 2_EvenFib/main_test.go
@@ -1,1 +1,24 @@
 package main
+
+import "testing"
+
+func Test_SumOfEvenFibsUnder(t *testing.T) {
+	tt := []struct {
+		limit    int
+		expected int
+	}{
+		{3, 2},
+		{8, 10},
+		{33, 10},
+		{34, 44},
+	}
+
+	for _, test := range tt {
+		result := SumOfEvenFibsUnder(test.limit)
+
+		if result != test.expected {
+			t.Errorf("Expected %d, but was %d", test.expected, result)
+		}
+	}
+
+}
